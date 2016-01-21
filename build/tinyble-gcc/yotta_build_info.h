9fea1a0807ada15e540dd65cf98ec0eeb131f232
Jacob Rosenthal <jakerosenthal@gmail.com>
2016-01-21 00:58:35 -0700

    generated

--- build/tinyble-gcc/yotta_build_info.h
+++ build/tinyble-gcc/yotta_build_info.h
@@ -5,9 +5,9 @@
 #define YOTTA_BUILD_MONTH 1 // UTC month 1-12
 #define YOTTA_BUILD_DAY 21 // UTC day 1-31
 #define YOTTA_BUILD_HOUR 7 // UTC hour 0-24
-#define YOTTA_BUILD_MINUTE 48 // UTC minute 0-59
-#define YOTTA_BUILD_SECOND 47 // UTC second 0-61
-#define YOTTA_BUILD_UUID 8fcdf0c4-9b93-406b-8bf4-309c0d4136ec // unique random UUID for each build
-#define YOTTA_BUILD_VCS_ID ca6fd7d623fb226aefefa8be2d5c0894f9aadbcc // git or mercurial hash
+#define YOTTA_BUILD_MINUTE 57 // UTC minute 0-59
+#define YOTTA_BUILD_SECOND 19 // UTC second 0-61
+#define YOTTA_BUILD_UUID 3e2b0d89-730a-4854-9c41-95c15f59697f // unique random UUID for each build
+#define YOTTA_BUILD_VCS_ID 5317117d9d0886233777565701f284e7a26244c8 // git or mercurial hash
 #define YOTTA_BUILD_VCS_CLEAN 0 // evaluates true if the version control system was clean, otherwise false
 #endif // ndef __YOTTA_BUILD_INFO_H__
